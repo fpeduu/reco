f960d3b91cdd9abbe3fb780223e5d9679eff40d0
JDaniloC <jdsc@cin.ufpe.br>
2023-08-15 11:11:51 -0300

    Add Header/Footer components closes #3

---
+++ nextjs_frontend/src/components/Header/header.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+export default function Header() {
+    return (
+        <div className='flex justify-around border-b border-gray-300 p-6'>
+            <span className='flex place-items-center gap-8 font-medium'>
+                <Image
+                    className="mr-10"
+                    src="/vercel.svg"
+                    alt="Vercel Logo"
+                    width={100}
+                    height={24}
+                    priority
+                />
+                <a href="/">
+                    Página Inicial
+                </a>
+                <a href="/config/">
+                    Configurações
+                </a>
+            </span>
+            <span className='flex place-items-center gap-2 font-medium'>
+                <Image
+                    src="/icons/logout.svg"
+                    className="dark:invert"
+                    alt="Logout Logo"
+                    width={24}
+                    height={24}
+                />
+                <button className="underline">
+                    Sair
+                </button>
+            </span>
+        </div>
+    )
+}
