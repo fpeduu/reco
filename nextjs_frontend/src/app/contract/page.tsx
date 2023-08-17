c9c03e350408b38676302c4158472dcb0b0fad1c
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-08-16 21:14:33 -0300

    Chore: change type of Acordo's diaPagamento field

--- nextjs_frontend/src/app/contract/page.tsx
+++ nextjs_frontend/src/app/contract/page.tsx
@@ -2,56 +2,56 @@ import { Acordo } from "@/models/Acordos";
 import { serverURL } from "../../config";
 
 async function fetchContracts() {
-    const response = await fetch(`${serverURL}/contract/api/`);
-    const contractList: Acordo[] = await response.json();
-    return contractList;
+  const response = await fetch(`${serverURL}/contract/api/`);
+  const contractList: Acordo[] = await response.json();
+  return contractList;
 }
 
 async function createContract() {
-    const response = await fetch(`${serverURL}/contract/api/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            id: 0,
-            descricao: "Teste",
-            devedorCpf: "12345678910",
-            diaPagamento: new Date(),
-            valor: 1000,
-            status: "Pendente",
-            juros: 0.1,
-            qtdParcelas: 10,
-            usuarioEmail: "teste@email.com",
-        })
-    });
-    const newContract: Acordo = await response.json();
-    return newContract;
+  const response = await fetch(`${serverURL}/contract/api/`, {
+    method: "POST",
+    body: JSON.stringify({
+      id: 0,
+      descricao: "Teste",
+      devedorCpf: "12345678910",
+      diaPagamento: 1,
+      valor: 1000,
+      status: "Pendente",
+      juros: 0.1,
+      qtdParcelas: 10,
+      usuarioEmail: "teste@email.com"
+    })
+  });
+  const newContract: Acordo = await response.json();
+  return newContract;
 }
 
 async function deleteContracts() {
-    const response = await fetch(`${serverURL}/contract/api/`, {
-        method: 'DELETE',
-        body: JSON.stringify({})
-    });
-    const deletedContracts: Acordo[] = await response.json();
-    return deletedContracts;
+  const response = await fetch(`${serverURL}/contract/api/`, {
+    method: "DELETE",
+    body: JSON.stringify({})
+  });
+  const deletedContracts: Acordo[] = await response.json();
+  return deletedContracts;
 }
 
 export default async function ContractPage() {
-    const contracts = await fetchContracts();
+  const contracts = await fetchContracts();
 
-    return (
-        <div>
-            <h1>Contratos</h1>
-            {contracts.map((contract) => (
-                <div key={contract.id}>
-                    <p>CPF: {contract.devedorCpf}</p>
-                    <p>Descrição: {contract.descricao}</p>
-                    <p>Valor: R${contract.valor}</p>
-                    <p>Status: {contract.status}</p>
-                    <p>Juros: {contract.juros * 100}%</p>
-                    <p>Parcelas: {contract.qtdParcelas}</p>
-                    <p>Administrador(a): {contract.usuarioEmail}</p>
-                </div>
-            ))}
+  return (
+    <div>
+      <h1>Contratos</h1>
+      {contracts.map((contract) => (
+        <div key={contract.id}>
+          <p>CPF: {contract.devedorCpf}</p>
+          <p>Descrição: {contract.descricao}</p>
+          <p>Valor: R${contract.valor}</p>
+          <p>Status: {contract.status}</p>
+          <p>Juros: {contract.juros * 100}%</p>
+          <p>Parcelas: {contract.qtdParcelas}</p>
+          <p>Administrador(a): {contract.usuarioEmail}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
