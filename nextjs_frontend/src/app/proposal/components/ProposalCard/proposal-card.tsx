c067234cf741fb22362ed081f9255af061be911a
JDaniloC <jdsc@cin.ufpe.br>
2023-08-17 14:57:01 -0300

    Move ProposalCard to another component

---
+++ nextjs_frontend/src/app/proposal/components/ProposalCard/proposal-card.tsx
@@ -0,0 +1,57 @@
+import { Acordo } from "@/models/Acordos";
+import Styles from "./proposal-card.module.scss";
+
+interface ProposalCardProps {
+  proposal: Acordo;
+  position: number;
+  isSelected: boolean;
+  onSelected: (proposalID: number) => void;
+}
+
+export default function ProposalCard({
+  proposal,
+  position,
+  isSelected,
+  onSelected,
+}: ProposalCardProps) {
+  function formatJuros(juros: number) {
+    return juros !== 0 ? `${juros * 100}%` : "Sem taxa de juros";
+  };
+
+  const handleSelect = () => {
+    onSelected(proposal.id);
+  };
+
+  return (
+    <div className="w-1/5 flex flex-col items-center justify-center">
+      <div
+        className={Styles.proposalCard}
+        data-selected={isSelected}
+      >
+        <span className="font-medium mb-1">
+          <span className="font-extrabold text-xl">
+            {position}º&nbsp;
+          </span>
+          melhor Acordo
+        </span>
+        <span className="font-bold text-4xl">R$ {proposal.valor}</span>
+        <span className="font-medium text-[#969696] text-sm text-center">p/ mês</span>
+        <span className="py-5 font-medium text-[#969696] whitespace-pre text-sm text-center">
+          {proposal.descricao}
+        </span>
+        <ul className="font-medium text-sm text-[#969696]">
+          <li>Valor: {proposal.valor} reais</li>
+          <li>Parcela dividida: {proposal.qtdParcelas} meses</li>
+          <li>Pagamento: Mensalmente</li>
+          <li>Data de pagamento: Dia {proposal.diaPagamento}</li>
+          <li>Juros: {formatJuros(proposal.juros)}</li>
+        </ul>
+      </div>
+      <button
+        data-selected={isSelected}
+        onClick={handleSelect}>
+        Selecionar
+      </button>
+    </div>
+  );
+};
--- nextjs_frontend/src/app/proposal/components/ProposalPodium/proposal-podium.tsx
+++ nextjs_frontend/src/app/proposal/components/ProposalPodium/proposal-podium.tsx
@@ -1,98 +1,54 @@
 "use client";
+
 import { useState } from "react";
 import { Acordo } from "@/models/Acordos";
+import ProposalCard from "../ProposalCard/proposal-card";
 import Styles from "./proposal-podium.module.scss";
 
-const ProposalCard = ({
-  isSelected,
-  onClick,
-  index,
-  position,
-  proposal
-}: {
-  isSelected: boolean;
-  onClick: Function;
-  index: number;
-  position: number;
-  proposal: Acordo;
-}) => {
-  return (
-    <div className="w-1/5 flex flex-col items-center justify-center">
-      <div className={Styles.proposalCard + " " + (isSelected ? Styles.selected : "")}>
-        <span className="font-medium mb-1">
-          <span className="font-extrabold text-xl">{position}º</span>melhor Acordo
-        </span>
-        <span className="font-bold text-4xl">R$ {proposal.valor}</span>
-        <span className="font-medium text-[#969696] text-sm text-center">p/ mês</span>
-        <span className="py-5 font-medium text-[#969696] whitespace-pre text-sm text-center">
-          {proposal.descricao}
-        </span>
-        <ul>
-          <li>Valor: {proposal.valor} reais</li>
-          <li>Parcela dividida: {proposal.qtdParcelas} meses</li>
-          <li>Pagamento: Mensalmente</li>
-          <li>Data de pagamento: Dia {proposal.diaPagamento}</li>
-          <li>
-            Juros:{" "}
-            {proposal.juros !== 0 ? `${proposal.juros * 100}%` : "Sem taxa de juros"}
-          </li>
-        </ul>
-      </div>
-      <button
-        className={isSelected ? Styles.active : ""}
-        onClick={() => onClick(index, position - 1)}>
-        Selecionar
-      </button>
-    </div>
-  );
-};
-
-const CardList = ({
-  proposals,
-  selectedCard,
-  handleCardClick
-}: {
+interface ProposalPodiumProps {
   proposals: Acordo[];
-  selectedCard: number;
-  handleCardClick: Function;
-}) => {
-  return (
-    <>
-      {[proposals[1], proposals[0], proposals[2]].map((proposal, index) => (
-        <ProposalCard
-          key={index}
-          index={index}
-          isSelected={selectedCard === index}
-          onClick={handleCardClick}
-          position={proposals.indexOf(proposal) + 1}
-          proposal={proposal}
-        />
-      ))}
-    </>
-  );
-};
+  changeProposal: (proposal: Acordo) => void;
+}
 
 export default function ProposalPodium({
   proposals,
   changeProposal
-}: {
-  proposals: Acordo[];
-  changeProposal: Function;
-}) {
-  const [selectedCard, setSelectedCard] = useState(1);
+}: ProposalPodiumProps) {
+  const [selectedCardID, setSelectedCardID] = useState<number>();
 
-  const handleCardClick = (index: number, position: number) => {
-    setSelectedCard(index);
-    changeProposal(proposals[position]);
+  function proposalsSorted(): Array<[number, Acordo]> {
+    const middleIndex = Math.floor(proposals.length / 2);
+    const output = new Array(proposals.length).fill(null);
+
+    for (let i = 0; i < proposals.length; i++) {
+      const isEven = i % 2 === 0;
+      const divided = i - Math.floor(i / 2);
+      const index = isEven ? middleIndex + divided
+                           : middleIndex - divided;
+      output[index] = [i + 1, proposals[i]];
+    }
+
+    return output;
+  }
+
+  const handleCardClick = (proposalID: number) => {
+    const selectedProposal = proposals.filter(
+      (proposal) => proposal.id === proposalID)[0];
+    setSelectedCardID(proposalID);
+    changeProposal(selectedProposal);
   };
 
   return (
     <div className={Styles.cardWrapper}>
-      <CardList
-        selectedCard={selectedCard}
-        handleCardClick={handleCardClick}
-        proposals={proposals}
-      />
+      {proposalsSorted().map(([position, proposal]) => (
+        <ProposalCard
+          key={proposal.id}
+          proposal={proposal}
+          position={position}
+          onSelected={handleCardClick}
+          isSelected={selectedCardID === proposal.id}
+        />
+      ))}
     </div>
   );
-}
+};
