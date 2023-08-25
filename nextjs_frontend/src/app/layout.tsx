e75f17053b2722b5bbb3deab6891d6abcfddd3f4
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-08-24 21:14:36 -0300

    feat: add ChoosenProposalContext

--- nextjs_frontend/src/app/layout.tsx
+++ nextjs_frontend/src/app/layout.tsx
@@ -1,34 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header/header';
-import Footer from '@/components/Footer/footer';
-import visbyCF from '@/config/fonts';
-import { NextAuthProvider } from './providers';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header/header";
+import Footer from "@/components/Footer/footer";
+import visbyCF from "@/config/fonts";
+import { NextAuthProvider } from "./providers";
+import { ChoosenProposalProvider } from "./proposal/[cpfDevedor]/contexts/ChoosenProposalContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Reco',
-  description: 'Uma plataforma de resolução de acordos',
-}
+  title: "Reco",
+  description: "Uma plataforma de resolução de acordos"
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${visbyCF.className} ${inter.className}`}>
         <NextAuthProvider>
-          <Header/>
-          <main className="flex min-h-screen flex-col items-center justify-between">
-            {children}
-          </main>
-          <Footer/>
+          <ChoosenProposalProvider>
+            <Header />
+            <main className="flex min-h-screen flex-col items-center justify-between">
+              {children}
+            </main>
+            <Footer />
+          </ChoosenProposalProvider>
         </NextAuthProvider>
       </body>
     </html>
-  )
+  );
 }
