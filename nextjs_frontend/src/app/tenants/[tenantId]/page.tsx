3d53fc4734e0e8567cac613df02ba43ed2528ecf
karen <ksbs@cin.ufpe.br>
2023-08-27 11:51:31 -0300

    add params to confirmation of debtors page

--- nextjs_frontend/src/app/tenants/[tenantId]/page.tsx
+++ nextjs_frontend/src/app/tenants/[tenantId]/page.tsx
@@ -1,9 +1,20 @@
-import AuthTitle from "@/components/AuthTItle/auth-title";
-import React from "react";
+"use client";
 import DebtorCard from "../components/DebtorCard/debtor-card";
-// import Styles from "./agreements.module.css";
+import { useRouter } from "next/navigation";
+import { serverURL } from "@/config";
 
-export default function page() {
+interface QueryParameters {
+  params: {
+    condominiumName: string;
+    lateTuitions: number;
+    debtorName: string;
+    debtorCPF: string;
+  };
+}
+
+export default function ConfirmationPage({ params }: QueryParameters) {
+  const router = useRouter();
+
   return (
     <div className="containerLayout">
       <div className="flex flex-col justify-center gap-5 m-auto h-full max-w-3xl">
@@ -14,21 +25,28 @@ export default function page() {
           Você selecionou o seguinte inadimplente:
         </h2>
         <DebtorCard
-          key={123}
-          debtorCPF={"123"}
-          debtorName={"name"}
-          condominiumName={"condominium"}
-          lateTuitions={123}
+          debtorCPF={params.debtorCPF}
+          debtorName={params.debtorName}
+          condominiumName={params.condominiumName}
+          lateTuitions={params.lateTuitions}
           chosen={true}
         />
         <h2 className="text-xl font-medium leading-10">
           É com esta pessoa que deseja iniciar o acordo?{" "}
         </h2>
         <div className="flex flex-row justify-center gap-5 max-w-lg	">
-          <button className="w-1/2 py-3 px-2 rounded-full text-tertiary text-s font-medium text-center bg-secondary">
+          <button
+            onClick={() =>
+              router.push(`${serverURL}/proposal/${params.debtorCPF}`)
+            }
+            className="w-1/2 py-3 px-2 rounded-full text-tertiary text-s font-medium text-center bg-secondary"
+          >
             Sim, iniciar acordo
           </button>
-          <button className="w-1/2 py-3 px-2 rounded-full text-tertiary text-s font-medium text-center bg-[#ADADAD]">
+          <button
+            onClick={() => router.back()}
+            className="w-1/2 py-3 px-2 rounded-full text-tertiary text-s font-medium text-center bg-[#ADADAD]"
+          >
             Não, escolher outra
           </button>
         </div>
