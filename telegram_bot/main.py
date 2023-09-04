f4184eaed50398002a57ca35d8a7d8bd8b38203d
JDaniloC <jdsc@cin.ufpe.br>
2023-09-04 15:54:34 -0300

    Bugfix: Fix requirements

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -3,11 +3,13 @@ from models import MemoryAgreement
 from messages import *
 from utils import *
 from api import *
+
+from flask import Flask,request
 import telebot
 
 chats_memory: dict[str, MemoryAgreement] = dict()
 bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
-bot.delete_webhook()
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def handler_0_start_conversation(message):
@@ -173,4 +175,20 @@ def callback_5_handle_modification(call):
 def echo_all(message):
     bot.send_message(message.chat.id, ECHO_MSG)
 
-bot.polling(none_stop=True)
+@server.route(f'/{BOT_TOKEN}/', methods=['POST'])
+def getMessage():
+    bot.process_new_updates([
+        telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+    ])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.set_webhook(
+        url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{BOT_TOKEN}",
+        secret_token=SECRET
+    )
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=PORT)
