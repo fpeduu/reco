d403bdedff2e33bd44b08ed582a6635682839517
JDaniloC <jdsc@cin.ufpe.br>
2023-09-04 15:08:05 -0300

    Force telegram bot PORT on heroku

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 SERVER_URL = os.environ["SERVER_URL"]
 SITE_URL = os.environ["SITE_URL"]
 
-PORT = int(os.environ.get("PORT", 8443))
+PORT = 8443
 SECRET = os.environ.get("SECRET", "secret")
 HEROKU_APP_NAME = os.environ["HEROKU_APP_NAME"]
