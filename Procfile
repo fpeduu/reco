53cee60a799049a039b2d9614e21f80cf791320b
JDaniloC <jdsc@cin.ufpe.br>
2023-09-04 12:01:44 -0300

    BugFix: Deploy heroku telegram 2

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 server: cd fastapi_backend && uvicorn main:app --host 0.0.0.0 --port $PORT
-telegram: cd telegram_bot && uvicorn main:app
+telegram: cd telegram_bot && python main.py
--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 SERVER_URL = os.environ["SERVER_URL"]
 SITE_URL = os.environ["SITE_URL"]
 
-PORT = 8443
+PORT = int(os.environ.get("PORT", 8443))
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 SECRET = os.environ.get("SECRET", "secret")
