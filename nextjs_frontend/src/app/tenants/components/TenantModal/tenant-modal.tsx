2f7ec5f087134749b9f4027e1feb787f8ab2da6a
karen <ksbs@cin.ufpe.br>
2023-09-05 21:57:51 -0300

    <add> close button

--- nextjs_frontend/src/app/tenants/components/TenantModal/tenant-modal.tsx
+++ nextjs_frontend/src/app/tenants/components/TenantModal/tenant-modal.tsx
@@ -29,15 +29,9 @@ const TenantModal: React.FC<TenantModalProps> = ({
   function handleStartAgreement() {
     router.push(`${serverURL}/proposal/${debtor.cpf}`);
   }
-  // const modalClasses = `fixed inset-0 flex items-center justify-center z-50 ${
-  //   open ? "opacity-100" : "opacity-0 pointer-events-none"
-  // }`;
-  // const modalContentClasses = "bg-white w-96 p-6 rounded shadow-md";
-  // const closeButtonClasses =
-  //   "absolute top-2 right-2 text-gray-500 hover:text-gray-800 cursor-pointer";
+
   const closeModal = () => {
-    console.log("Close modal function called from modal");
-    onClose(); // Check if this is called correctly
+    onClose();
   };
   return (
     <div>
@@ -72,25 +66,15 @@ const TenantModal: React.FC<TenantModalProps> = ({
           From: "opacity-100 translate-y-0 sm:scale-100"
           To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       --> */}
-              <div className="relative p-8 transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-5xl">
+              <div className="relative p-8 transform overflow-hidden rounded-2xl bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-5xl">
                 <div className="bg-white sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <svg
-                        className="h-6 w-6 text-red-600"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                        />
-                      </svg>
-                    </div> */}
+                  <div className="sm:flex sm:items-start ">
+                    <button
+                      onClick={closeModal}
+                      className="absolute top-2 right-6 text-5xl h-0 text-gray-500 hover:text-gray-700"
+                    >
+                      &times;
+                    </button>
                     <div className="mt-3 gap-5 w-full text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <h1
                         className="font-bold text-3xl leading-10"
