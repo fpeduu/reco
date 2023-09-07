92db727f76fffa40736fb571e138f757d1a121f9
karen <ksbs@cin.ufpe.br>
2023-09-07 17:51:03 -0300

    <add> section features to lading page

--- nextjs_frontend/src/app/page.tsx
+++ nextjs_frontend/src/app/page.tsx
@@ -12,21 +12,83 @@ export default function Home() {
     //     priority
     //   />
     // </div>
-    <section id="home" className="w-full max-h-fit bg-secondary">
-      <div className="text-tertiary p-16 flex flex-col gap-4 mb-4">
-        <h1 className="text-7xl">Boas-vindas à Reco!</h1>
-        <p className="max-w-2xl tracking-wider leading-7">
-          É um prazer te ter conosco! Somos um serviço especializado em
-          intermediar acordos extrajudiciais de cobranças condominiais de forma
-          ágil e eficiente. Nossa plataforma foi projetada para atender às
-          necessidades de administradoras, conselheiros fiscais, síndicos e
-          condôminos, oferecendo uma abordagem inovadora e focada na resolução
-          pacífica de conflitos.
-        </p>
-        <button className="bg-primary rounded-full max-w-[252px] h-12">
-          Junte-se à Reco
-        </button>
-      </div>
-    </section>
+    <div className="flex flex-col w-full max-h-fit">
+      <section id="home" className="w-full max-h-fit bg-secondary">
+        <div className="text-tertiary p-16 flex flex-col gap-4 mb-4 items-center lg:items-start">
+          <h1 className="text-7xl">Boas-vindas à Reco!</h1>
+          <p className="max-w-2xl tracking-wider leading-7">
+            É um prazer te ter conosco! Somos um serviço especializado em
+            intermediar acordos extrajudiciais de cobranças condominiais de
+            forma ágil e eficiente. Nossa plataforma foi projetada para atender
+            às necessidades de administradoras, conselheiros fiscais, síndicos e
+            condôminos, oferecendo uma abordagem inovadora e focada na resolução
+            pacífica de conflitos.
+          </p>
+          <button className="bg-primary rounded-full w-[252px] h-12">
+            Junte-se à Reco
+          </button>
+        </div>
+      </section>
+      {/* SECTION Features*/}
+      <section id="features" className="w-full max-h-fit bg-tertiary">
+        <div className="flex p-20">
+          <div className="flex flex-wrap gap-5 items-center justify-center">
+            <h2 className="text-7xl md:text-8xl md:min-w-[550px]">
+              <strong className="text-primary">reco</strong> <br />
+              te oferece:
+            </h2>
+            <div className="flex flex-wrap items-center justify-center bg-secondary rounded-xl text-tertiary p-6 gap-5 md:min-w-[550px] min-h-[150px] shadow-md md:h-[150px] md:justify-start divide-x">
+              <Image
+                className="dark:invert"
+                alt="brain analysis icon"
+                src="/icons/brain_analysis.svg"
+                width={80}
+                height={80}
+                priority
+              />
+              <div className="flex flex-col gap-2 px-5 max-w-[300px]">
+                <h5 className="text-xl">Análises avançadas</h5>
+                <p className="text-[13px] leading-6">
+                  Nossa Inteligência Artificial fornece as melhores propostas às
+                  suas negociações.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-center bg-secondary rounded-xl text-tertiary p-6 gap-5 md:min-w-[550px] min-h-[150px] shadow-md md:h-[150px] md:justify-start divide-x">
+              <Image
+                className="dark:invert"
+                alt="laptopicon"
+                src="/icons/laptop_autom.svg"
+                width={80}
+                height={80}
+                priority
+              />
+              <div className="flex flex-col gap-2 px-5 max-w-[300px]">
+                <h5 className="text-xl">Automatização</h5>
+                <p className="text-[13px] leading-6">
+                  Todo o processo é automático e programado para sua satisfação.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-center bg-secondary rounded-xl text-tertiary p-8 gap-5 md:min-w-[550px] min-h-[150px] shadow-md md:h-[150px] md:justify-start divide-x">
+              <Image
+                className="dark:invert"
+                alt="security icon"
+                src="/icons/security.png"
+                width={70}
+                height={70}
+                priority
+              />
+              <div className="flex flex-col gap-2 px-5 max-w-[300px]">
+                <h5 className="text-xl">Sistema Seguro</h5>
+                <p className="text-[13px] leading-6">
+                  Tranquilize-se! Seus dados estarão bem seguros e armazenados.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
