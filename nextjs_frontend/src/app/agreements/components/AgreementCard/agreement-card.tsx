97d6d8658196743da5eadc918c5d05c7305fc5d4
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-16 19:35:18 -0300

    fix: add "Atraso" data and filter to agreements

--- nextjs_frontend/src/app/agreements/components/AgreementCard/agreement-card.tsx
+++ nextjs_frontend/src/app/agreements/components/AgreementCard/agreement-card.tsx
@@ -1,23 +1,29 @@
 import { AcordoIdentificado } from "@/models/Acordos";
 import StatusBar from "../StatusBar/status-bar";
 import Link from "next/link";
+import { Condomino } from "@/models/Devedores";
 
 interface AgreementCardProps {
   agreement: AcordoIdentificado;
+  tenant: Condomino | null;
 }
 
-export default function AgreementCard({ agreement }: AgreementCardProps) {
+export default function AgreementCard({ agreement, tenant }: AgreementCardProps) {
   const isConcluded = agreement.status === "Negociação concluída";
   const linkPath = isConcluded ? `/proposal/${agreement.cpfDevedor}` : `#`;
 
   return (
-    <Link className="w-1/3 p-5 cursor-pointer" href={linkPath}>
+    <Link className="w-full cursor-pointer" href={linkPath}>
       <div className="p-5 flex flex-col rounded-lg text-sm bg-white hover:bg-gray-50">
         <span className="text-xl font-bold">{agreement.nomeDevedor}</span>
         <span className="mb-2 text-zinc-500">{agreement.cpfDevedor}</span>
         <span>
           <span className="font-semibold">Local:&nbsp;</span>
           {agreement.nomeCondominio}
+        </span>
+        <span>
+          <span className="font-semibold">Atraso:&nbsp;</span>
+          {tenant?.mensalidadesAtrasadas ? `${tenant.mensalidadesAtrasadas} meses` : "-"}
         </span>
         <StatusBar status={agreement.status} />
       </div>
--- nextjs_frontend/src/app/agreements/page.tsx
+++ nextjs_frontend/src/app/agreements/page.tsx
@@ -5,6 +5,7 @@ import { AcordoIdentificado } from "@/models/Acordos";
 import AgreementList from "./components/AgreementList/agreement-list";
 import Search from "@/components/Search/search";
 import { useEffect, useState } from "react";
+import { Condomino } from "@/models/Devedores";
 
 const BASE_URL = `${serverURL}/api/agreements/`;
 
@@ -17,14 +18,23 @@ const fetchAgreements = async () => {
     })) as AcordoIdentificado[];
 };
 
+async function fetchTenants() {
+  return (await fetch(`${serverURL}/api/tenants/`)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error(error);
+      return [] as Condomino[];
+    })) as Condomino[];
+}
+
 export default function AgreementsPage() {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [agreements, setAgreements] = useState<AcordoIdentificado[]>([]);
+  const [tenants, setTenants] = useState<Condomino[]>([]);
 
   useEffect(() => {
     fetchAgreements().then((agreements) => setAgreements(agreements));
-    // @ts-ignore
-    import("preline");
+    fetchTenants().then((tenants) => setTenants(tenants));
   }, []);
 
   const inProgressAgreements = agreements.filter((agreement) => {
@@ -41,6 +51,7 @@ export default function AgreementsPage() {
       <AgreementList
         searchQuery={searchQuery}
         agreements={inProgressAgreements}
+        tenants={tenants}
         title="Em andamento"
         description="Confira suas negociações em andamento. Clique nos cards para mais informações."
         filterByProgress={true}
@@ -48,6 +59,7 @@ export default function AgreementsPage() {
       <AgreementList
         searchQuery={searchQuery}
         agreements={endedAgreements}
+        tenants={tenants}
         title="Finalizadas"
         description="Confira suas negociações finalizadas. Clique nos cards para mais informações."
         filterByProgress={false}
