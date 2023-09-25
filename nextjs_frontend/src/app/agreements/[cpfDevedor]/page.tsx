8e6a8301c3db5e60c11484e795a1162b43a012e1
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-24 21:26:56 -0300

    feat: update download button styles

--- nextjs_frontend/src/app/agreements/[cpfDevedor]/page.tsx
+++ nextjs_frontend/src/app/agreements/[cpfDevedor]/page.tsx
@@ -84,7 +84,7 @@ export default function AgreementStatus() {
             Detalhes
           </button>
         </nav>
-        <div className="p-20 flex flex-col items-end">
+        <div className="p-20 flex flex-col items-end gap-10">
           {subpage === "timeline" && <StatusBarBig status={agreement.status} />}
           {subpage === "timeline" &&
             (agreement.status === "Negociação concluída" ||
