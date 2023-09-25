99713aa5c939c539f397b9c96e7483dca9a48c9d
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-24 21:21:02 -0300

    fix: change tenant profile card title weight

--- nextjs_frontend/src/app/agreements/[cpfDevedor]/components/TenantProfileCard/tenant-profile-card.tsx
+++ nextjs_frontend/src/app/agreements/[cpfDevedor]/components/TenantProfileCard/tenant-profile-card.tsx
@@ -7,7 +7,7 @@ interface TenantProfileCardProps {
 export default function TenantProfileCard({ tenant }: TenantProfileCardProps) {
   return (
     <div className="w-1/3 h-40 py-6 px-10 flex flex-col justify-between rounded-2xl bg-white">
-      <span className="text-xl font-semibold">Perfil do inadimplente</span>
+      <span className="text-xl font-bold">Perfil do inadimplente</span>
       <div className="flex items-center justify-between">
         <div className="flex flex-col justify-between">
           <span className="text-lg font-medium">{tenant.nome}</span>
