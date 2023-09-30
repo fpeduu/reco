f951870d86f90de4eab1db47ee274648367361ae
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-30 17:06:06 -0300

    fix: agreement card styles

--- nextjs_frontend/src/app/agreements/components/AgreementCard/agreement-card.tsx
+++ nextjs_frontend/src/app/agreements/components/AgreementCard/agreement-card.tsx
@@ -8,24 +8,19 @@ interface AgreementCardProps {
 
 export default function AgreementCard({ agreement }: AgreementCardProps) {
   return (
-    <Link
-      className="w-full cursor-pointer"
-      href={`/agreements/${agreement.cpf}`}
-    >
-      <div className="p-5 flex flex-col rounded-xl text-sm bg-white hover:bg-gray-50">
-        <span className="text-xl font-semibold mb-5">
-          {agreement.nome}
-        </span>
+    <Link className="w-full cursor-pointer" href={`/agreements/${agreement.cpf}`}>
+      <div className="p-5 flex flex-col rounded-xl text-sm font-light bg-white hover:bg-gray-50">
+        <span className="text-xl font-normal mb-5">{agreement.nome}</span>
         <span>
-          <span className="font-semibold">CPF:</span>&nbsp;
+          <span className="font-normal">CPF:</span>&nbsp;
           {agreement.cpf}
         </span>
         <span>
-          <span className="font-semibold">Condomínio:&nbsp;</span>
+          <span className="font-normal">Condomínio:&nbsp;</span>
           {agreement.nomeCondominio}
         </span>
         <span className="mb-5">
-          <span className="font-semibold">Atraso:&nbsp;</span>
+          <span className="font-normal">Atraso:&nbsp;</span>
           {agreement.mensalidadesAtrasadas} meses de atraso
         </span>
         <StatusBar status={agreement.acordo.status} />
