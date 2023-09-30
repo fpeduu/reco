bde2e85d399878348014ef377f55e4cfec016939
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-30 15:27:01 -0300

    fix: add valorTotal when creating new agreement

--- nextjs_frontend/src/app/tenants/components/TenantModal/tenant-modal.tsx
+++ nextjs_frontend/src/app/tenants/components/TenantModal/tenant-modal.tsx
@@ -17,46 +17,53 @@ interface TenantModalProps {
 }
 
 async function fetchProposalInfos(cpf: string) {
-  return await fetch(`${serverURL}/api/tenants/${cpf}/`)
+  return (await fetch(`${serverURL}/api/tenants/${cpf}/`)
     .then((response) => response.json())
     .catch((error) => {
       console.error(error);
-      return null 
-    }) as RegrasProposta | null;
+      return null;
+    })) as RegrasProposta | null;
 }
 
 async function createAgreement(
   cpf: string,
   entrada: number,
   valorParcela: number,
+  valorTotal: number,
   qtdParcelas: number
 ) {
-  return await fetch(`${serverURL}/api/agreements/${cpf}/`, {
+  return (await fetch(`${serverURL}/api/agreements/${cpf}/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ entrada, valorParcela, qtdParcelas })
-  }).then((response) => response.json())
+    body: JSON.stringify({ entrada, valorParcela, valorTotal, qtdParcelas })
+  })
+    .then((response) => response.json())
     .catch((error) => {
-        console.error(error);
-        return null 
-    }) as Acordo | null;
+      console.error(error);
+      return null;
+    })) as Acordo | null;
 }
 
-export default function TenantModal({
-  open, onClose, debtor
-}: TenantModalProps) {
+export default function TenantModal({ open, onClose, debtor }: TenantModalProps) {
   const [confirmed, setConfirmed] = useState<boolean>(false);
   const [negotiation, setNegotiation] = useState<INegotiationData>({
-    bestValue: 0, worstValue: 0, bestInstallments: 0,
-    worstInstallments: 0, piorParcela: 0, melhorParcela: 0
+    bestValue: 0,
+    worstValue: 0,
+    bestInstallments: 0,
+    worstInstallments: 0,
+    piorParcela: 0,
+    melhorParcela: 0
   });
 
   async function onConfirm() {
-    createAgreement(debtor.cpf, negotiation.bestValue,
-                    negotiation.bestInstallments,
-                    negotiation.melhorParcela as number
+    createAgreement(
+      debtor.cpf,
+      negotiation.bestValue,
+      negotiation.bestInstallments,
+      debtor.valorDivida,
+      negotiation.melhorParcela as number
     ).then((data) => {
       if (data) {
         setConfirmed(true);
@@ -68,61 +75,65 @@ export default function TenantModal({
   }
 
   useEffect(() => {
-    if (open) fetchProposalInfos(debtor.cpf).then((data) => {
-      if (data) {
-        const value = debtor.valorDivida;
-        let bestValue = 0, worstValue = 0;
-        if (data.melhorEntrada) {
-          bestValue = data.melhorEntrada * value;
+    if (open)
+      fetchProposalInfos(debtor.cpf).then((data) => {
+        if (data) {
+          const value = debtor.valorDivida;
+          let bestValue = 0,
+            worstValue = 0;
+          if (data.melhorEntrada) {
+            bestValue = data.melhorEntrada * value;
+          }
+          if (data.piorEntrada) {
+            worstValue = data.piorEntrada * value;
+          }
+          setNegotiation({
+            bestValue,
+            worstValue,
+            piorParcela: data.piorParcela,
+            melhorParcela: data.melhorParcela,
+            bestInstallments: (value - bestValue) / data.melhorParcela,
+            worstInstallments: (value - worstValue) / data.piorParcela
+          });
         }
-        if (data.piorEntrada) {
-          worstValue = data.piorEntrada * value;
-        }
-        setNegotiation({
-          bestValue,
-          worstValue,
-          piorParcela: data.piorParcela,
-          melhorParcela: data.melhorParcela,
-          bestInstallments: (value - bestValue) / data.melhorParcela,
-          worstInstallments: (value - worstValue) / data.piorParcela
-        })
-      }
-    });
+      });
   }, [open]);
-  
-  return open && (
-    <div
-      className="relative z-10"
-      aria-labelledby="modal-title"
-      role="dialog"
-      aria-modal="true">
 
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-75
-                      transition-opacity">
-      </div>
+  return (
+    open && (
+      <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75
+                      transition-opacity"></div>
 
-      <div className="fixed inset-0 z-10 h-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center
+        <div className="fixed inset-0 z-10 h-screen overflow-y-auto">
+          <div
+            className="flex min-h-full items-end justify-center p-4 text-center
                         sm:items-center sm:p-0">
-          <div className="relative p-6 transform overflow-hidden rounded-2xl
+            <div
+              className="relative p-6 transform overflow-hidden rounded-2xl
                         bg-white text-left shadow-xl transition-all sm:my-8
                           sm:w-full sm:max-w-4xl">
-            <button onClick={onClose}
-              className="absolute top-10 right-14 text-5xl h-0
+              <button
+                onClick={onClose}
+                className="absolute top-10 right-14 text-5xl h-0
                         text-gray-500 hover:text-gray-700">
-              &times;
-            </button>
-            {confirmed ?
-            <Confirmation cpfDevedor={debtor.cpf}/> : 
-            <ModalContent
-              debtor={debtor}
-              onClose={onClose}
-              onConfirm={onConfirm}
-              negotiationData={negotiation}
-            />}
+                &times;
+              </button>
+              {confirmed ? (
+                <Confirmation cpfDevedor={debtor.cpf} />
+              ) : (
+                <ModalContent
+                  debtor={debtor}
+                  onClose={onClose}
+                  onConfirm={onConfirm}
+                  negotiationData={negotiation}
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
-};
+}
