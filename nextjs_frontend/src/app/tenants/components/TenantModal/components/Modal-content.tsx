4dd1fbe03bcf478fb3d72e2da579f2a3b33101a3
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-10-01 05:43:48 -0300

    fix: modals adjustments

--- nextjs_frontend/src/app/tenants/components/TenantModal/components/Modal-content.tsx
+++ nextjs_frontend/src/app/tenants/components/TenantModal/components/Modal-content.tsx
@@ -25,24 +25,15 @@ export default function ModalContent({
   onClose,
   onConfirm,
   negotiationData,
-  debtor,
+  debtor
 }: ModalContentProps) {
-  const {
-    melhorParcela,
-    piorParcela,
-    bestValue,
-    worstValue,
-    bestInstallments,
-    worstInstallments,
-  } = negotiationData;
+  const { melhorParcela, piorParcela, bestValue, worstValue, bestInstallments, worstInstallments } =
+    negotiationData;
 
   return (
-    <div className="bg-white p-16 flex justify-center">
+    <div className="bg-white pt-16 md:p-16 flex justify-center">
       <div className="mt-3 max-w-2xl w-full gap-5 sm:ml-4 sm:mt-0 sm:text-left">
-        <h1
-          className="font-medium text-4xl leading-10 text-center mb-2"
-          id="modal-title"
-        >
+        <h1 className="font-medium text-4xl leading-10 text-center mb-2" id="modal-title">
           Confira as informações:
         </h1>
         <p className="text-base font-light text-center">
@@ -53,8 +44,7 @@ export default function ModalContent({
         <div className="w-full my-8 gap-2 flex flex-col">
           <div
             className="flex-1 p-4 gap-10 flex flex-wrap items-center
-                          min-h-max justify-start rounded-md shadow bg-tertiary"
-          >
+                          min-h-max justify-start rounded-md shadow bg-tertiary">
             <div className="flex flex-col items-start gap-2 mr-auto">
               <span className="font-normal text-lg">{debtor.nome}</span>
               <span className="text-sm font-light">
@@ -63,9 +53,7 @@ export default function ModalContent({
             </div>
             <div className="flex flex-col items-start gap-2">
               <span className="font-normal text-sm">Atraso:</span>
-              <span className="text-sm font-light">
-                {debtor.mensalidadesAtrasadas} meses
-              </span>
+              <span className="text-sm font-light">{debtor.mensalidadesAtrasadas} meses</span>
             </div>
             <div className="flex flex-col items-start gap-2">
               <span className="font-normal text-sm">Dívida:</span>
@@ -93,15 +81,13 @@ export default function ModalContent({
           <button
             onClick={onConfirm}
             className="md:w-1/2 py-3 px-2 rounded-full text-tertiary
-              md:text-lg font-medium text-center bg-secondary"
-          >
+              md:text-lg font-medium text-center bg-secondary">
             Confirmar
           </button>
           <button
             onClick={onClose}
             className="md:w-1/2 py-3 px-2 rounded-full text-tertiary
-              md:text-lg font-medium text-center bg-[#808080]"
-          >
+              md:text-lg font-medium text-center bg-[#808080]">
             Cancelar
           </button>
         </div>
