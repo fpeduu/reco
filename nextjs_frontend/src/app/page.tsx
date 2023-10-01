cf18673826601b315119947065ce25129832a994
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-10-01 05:41:12 -0300

    fix: landing page adjustments

--- nextjs_frontend/src/app/page.tsx
+++ nextjs_frontend/src/app/page.tsx
@@ -22,17 +22,15 @@ export default function Home() {
       {/* HOME SECTION */}
       <section
         id="home"
-        className="w-full md:h-232 px-10 md:px-60 bg-neutral-950 flex items-center md:bg-origin-content md:bg-[url(/images/some_guy.png)] bg-no-repeat bg-right-top bg-cover "
-      >
+        className="w-full md:h-232 px-10 md:px-60 bg-neutral-950 flex items-center md:bg-origin-content md:bg-[url(/images/some_guy.png)] bg-no-repeat bg-right-top bg-cover ">
         <div className="w-full h-full py-48 text-neutral-200 flex flex-col gap-4 mb-4 items-center lg:items-start">
           <h1 className="text-7xl font-medium">Boas-vindas à Reco!</h1>
           <p className="max-w-2xl tracking-wider leading-7 font-extralight">
-            É um prazer te ter conosco! Somos um serviço especializado em
-            intermediar acordos extrajudiciais de cobranças condominiais de
-            forma ágil e eficiente. Nossa plataforma foi projetada para atender
-            às necessidades de administradoras, conselheiros fiscais, síndicos e
-            condôminos, oferecendo uma abordagem inovadora e focada na resolução
-            pacífica de conflitos.
+            É um prazer te ter conosco! Somos um serviço especializado em intermediar acordos
+            extrajudiciais de cobranças condominiais de forma ágil e eficiente. Nossa plataforma foi
+            projetada para atender às necessidades de administradoras, conselheiros fiscais,
+            síndicos e condôminos, oferecendo uma abordagem inovadora e focada na resolução pacífica
+            de conflitos.
           </p>
           <button className="mt-10 bg-primary rounded-full w-[252px] h-12 font-medium">
             Junte-se à Reco
@@ -40,21 +38,12 @@ export default function Home() {
         </div>
       </section>
       {/* FEATURES SECTION*/}
-      <section
-        id="features"
-        className="w-full max-h-fit bg-neutral-200 relative"
-      >
+      <section id="features" className="w-full max-h-fit bg-neutral-200 relative">
         <div className="w-full h-208 absolute right-0 -top-96 lg:bg-[url(/images/grafismo3.svg)] bg-right-top bg-no-repeat bg-contain " />
         <div className="flex justify-center p-20 mt-32 mb-20">
           <div className="grid grid-cols-1 lg:grid-cols-2  gap-12 my-10">
             <div>
-              <Image
-                alt="reco logo"
-                src="/logo.svg"
-                width={400}
-                height={100}
-                priority
-              />
+              <Image alt="reco logo" src="/logo.svg" width={400} height={100} priority />
               <h2 className="text-5xl md:text-6xl md:min-w-[500px] text-end font-extralight pr-4">
                 te oferece:
               </h2>
@@ -85,9 +74,7 @@ export default function Home() {
             <div className={Styles.userGuideStep}>
               <div>1</div>
 
-              <p>
-                Escolha o inadimplente com quem deseja realizar uma negociação
-              </p>
+              <p>Escolha o inadimplente com quem deseja realizar uma negociação</p>
             </div>
             <div className="relative top-12 right-12 hidden md:block lg:right-[105px] md:top-16">
               <div className={`${Styles.horizontalLine} `}></div>
@@ -97,10 +84,7 @@ export default function Home() {
             </div>
             <div className={Styles.userGuideStep}>
               <div>2</div>
-              <p>
-                Aguarde nossa tecnologia gerar as melhores propostas para seu
-                acordo
-              </p>
+              <p>Aguarde nossa tecnologia gerar as melhores propostas para seu acordo</p>
             </div>
             <div className="relative top-12 right-12 hidden md:block lg:right-[105px] md:top-16">
               <div className={Styles.horizontalLine}></div>
@@ -120,15 +104,11 @@ export default function Home() {
       {/* SECTION DEPOSITIONS */}
       <section
         id="depositions"
-        className="w-full max-h-fit bg-neutral-950 bg-[url(/images/grafismo2.svg)] bg-no-repeat bg-left-bottom bg-origin-border"
-      >
+        className="w-full max-h-fit bg-neutral-950 bg-[url(/images/grafismo2.svg)] bg-no-repeat bg-left-bottom bg-origin-border">
         <div className="flex flex-col items-center gap-10 p-16 text-neutral-200">
-          <h2 className="text-7xl font-medium">
-            O que nossos clientes estão dizendo?
-          </h2>
+          <h2 className="text-7xl font-medium">O que nossos clientes estão dizendo?</h2>
           <p className="py-3 px-8 font-light text-center rounded-3xl bg-primary">
-            Veja o que alguns de nossos clientes estão comentando sobre suas
-            experiências na Reco.
+            Veja o que alguns de nossos clientes estão comentando sobre suas experiências na Reco.
           </p>
           <div className="flex flex-wrap justify-center mt-16 gap-16 lg:gap-6">
             <DepositionCard
@@ -154,38 +134,19 @@ export default function Home() {
       </section>
       {/* PRICING SECTION */}
       <section id="pricing" className="w-full max-h-fit bg-neutral-200">
-        <div className="flex flex-wrap items-center justify-center gap-20 px-16 py-32 bg-[url(/images/grafismo1.svg)] bg-no-repeat bg-right">
+        <div className="flex flex-wrap items-center justify-center gap-20 px-16 py-32 bg-[url(/images/grafismo1.svg)] bg-no-repeat bg-right-bottom md:bg-right">
           <div className="w-[350px]">
             <h2 className="mb-10 text-7xl text-justify font-medium">Planos</h2>
             <p className="text-justify text-lg text-secondary font-light">
-              Conheça os planos da nossa plataforma! <br /> <br />O plano Básico
-              é descomplicado, ideal para conhecer a plataforma. Já o plano
-              Premium oferece recursos avançados e suporte especializado para
-              acordos justos e sustentáveis.
+              Conheça os planos da nossa plataforma! <br /> <br />O plano Básico é descomplicado,
+              ideal para conhecer a plataforma. Já o plano Premium oferece recursos avançados e
+              suporte especializado para acordos justos e sustentáveis.
             </p>
             <h4 className="font-medium text-2xl mt-10">Métodos de pagamento</h4>
-            <div className="flex flex-wrap gap-9 mt-4 justify-center md:justify-start">
-              <Image
-                alt="paypal icon"
-                src="/icons/paypal.svg"
-                width={35}
-                height={35}
-                priority
-              />
-              <Image
-                alt="pix icon"
-                src="/icons/pix.svg"
-                width={40}
-                height={40}
-                priority
-              />
-              <Image
-                alt="visa icon"
-                src="/icons/visa.svg"
-                width={50}
-                height={50}
-                priority
-              />
+            <div className="flex flex-wrap gap-5 md:gap-9 mt-4 justify-center md:justify-start">
+              <Image alt="paypal icon" src="/icons/paypal.svg" width={35} height={35} priority />
+              <Image alt="pix icon" src="/icons/pix.svg" width={40} height={40} priority />
+              <Image alt="visa icon" src="/icons/visa.svg" width={50} height={50} priority />
               <Image
                 alt="mastercard icon"
                 src="/icons/mastercard.svg"
