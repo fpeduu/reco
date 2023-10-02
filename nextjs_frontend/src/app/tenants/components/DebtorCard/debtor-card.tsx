31a5309517ce4540a2cfba2209981aaa74972cc5
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-10-02 02:31:05 -0300

    fix: debtor card fix in small sizes

--- nextjs_frontend/src/app/tenants/components/DebtorCard/debtor-card.tsx
+++ nextjs_frontend/src/app/tenants/components/DebtorCard/debtor-card.tsx
@@ -12,29 +12,28 @@ export default function DebtorCard({ tenant, openModal }: DebtorCardProps) {
 
   return (
     <div
-      className="w-full py-4 px-8 flex flex-wrap items-center justify-between
+      className="w-full py-4 px-8 flex flex-col md:flex-row flex-wrap items-start md:items-center justify-between
                  rounded-xl shadow bg-white">
       <div className="flex flex-col items-start gap-1  w-full md:w-2/12">
-        <span className="font-normal text-xl w-full whitespace-nowrap
+        <span
+          className="font-normal text-xl w-full whitespace-nowrap
                          overflow-hidden text-ellipsis ">
           {tenant.nome}
         </span>
       </div>
       <div className="w-28 flex md:block items-center gap-2">
         <p className="font-normal">CPF:</p>
-        <span className="text-xs font-light whitespace-nowrap">
-          {tenant.cpf}
-        </span>
+        <span className="text-xs font-light whitespace-nowrap">{tenant.cpf}</span>
       </div>
-      <div className="w-36 pb-1">
+      <div className="w-36 pb-1 flex md:block items-center gap-2">
         <p className="font-normal">Condomínio:</p>
-        <div className="flex items-center gap-1 pt-1 text-xs font-light">
+        <div className="flex items-center gap-1 pt-1 text-xs font-light whitespace-nowrap">
           {tenant?.nomeCondominio}
         </div>
       </div>
-      <div className="w-20 pb-1">
+      <div className="w-20 pb-1 flex md:block items-center gap-2">
         <p className="font-normal">Atraso:</p>
-        <div className="flex items-center gap-1 text-xs font-light">
+        <div className="flex items-center gap-1 text-xs font-light whitespace-nowrap">
           <span className="pt-1">
             {tenant.mensalidadesAtrasadas > 0
               ? `${tenant.mensalidadesAtrasadas} meses`
@@ -42,20 +41,17 @@ export default function DebtorCard({ tenant, openModal }: DebtorCardProps) {
           </span>
         </div>
       </div>
-      <div className="w-20 pb-1">
+      <div className="w-20 pb-1 flex md:block items-center gap-2">
         <p className="font-normal">Dívida:</p>
-        <div className="flex items-center gap-1 text-xs font-light">
-          <span className="pt-1">
-            R$ {tenant.valorDivida?.toLocaleString("pt-BR")}
-          </span>
+        <div className="flex items-center gap-1 text-xs font-light whitespace-nowrap">
+          <span className="pt-1">R$ {tenant.valorDivida?.toLocaleString("pt-BR")}</span>
         </div>
       </div>
       <div className="w-44 flex items-center justify-end">
         <button
           className="w-full h-10  rounded-md text-white
                   text-s font-normal text-center bg-primary"
-          onClick={handleStartAgreement}
-        >
+          onClick={handleStartAgreement}>
           Negociar
         </button>
       </div>
