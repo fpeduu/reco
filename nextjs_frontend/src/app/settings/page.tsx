4af0457be00d045a0e0ec8772a4a9ede0be38e0c
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-10-07 01:57:39 -0300

    feat: add settings page

---
+++ nextjs_frontend/src/app/settings/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import ProfileSettings from "./ProfileSettings/profile-settings";
+
+export default function SettingsPage() {
+  const { data: session } = useSession({ required: true });
+  const [activeTab, setActiveTab] = useState<"profile" | "payment" | "language">("profile");
+
+  function switchToProfile() {
+    setActiveTab("profile");
+  }
+
+  function switchToPayment() {
+    setActiveTab("payment");
+  }
+
+  function switchToLanguage() {
+    setActiveTab("language");
+  }
+
+  return (
+    <div className="containerLayout">
+      <h1 className="text-4xl font-semibold leading-10 my-10">Configurações</h1>
+      <div className="w-full flex flex-col gap-10 divide-y divide-neutral-300">
+        <nav className="w-full p-3 md:p-0 md:h-16 flex flex-col md:flex-row items-center justify-center gap-2 md:gap-32 border border-neutral-300 rounded-lg">
+          <button
+            onClick={switchToProfile}
+            className={
+              "w-full md:w-fit h-10 px-5 flex items-center justify-center rounded-lg text-lg font-light whitespace-nowrap " +
+              (activeTab === "profile" ? "bg-white" : "text-zinc-400")
+            }>
+            Meu Perfil
+          </button>
+          <button
+            onClick={switchToPayment}
+            className={
+              "w-full md:w-fit h-10 px-5 flex items-center justify-center rounded-lg text-lg font-light whitespace-nowrap " +
+              (activeTab === "payment" ? "bg-white" : "text-zinc-400")
+            }>
+            Dados de Pagamento
+          </button>
+          <button
+            onClick={switchToLanguage}
+            className={
+              "w-full md:w-fit h-10 px-5 flex items-center justify-center rounded-lg text-lg font-light whitespace-nowrap " +
+              (activeTab === "language" ? "bg-white" : "text-zinc-400")
+            }>
+            Idioma
+          </button>
+        </nav>
+        {activeTab === "profile" && <ProfileSettings session={session} />}
+      </div>
+    </div>
+  );
+}
