27873cc97eb6f49e8d4bbe7b815c0aba983b58b5
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-10-07 01:52:53 -0300

    feat: add settings link to sidebar button

--- nextjs_frontend/src/components/SideBar/sidebar.tsx
+++ nextjs_frontend/src/components/SideBar/sidebar.tsx
@@ -18,26 +18,17 @@ export default function SideBar() {
           <nav>
             <div className="flex flex-col gap-2">
               {!hideSideBar && (
-                <span className="ml-3 text-slate-400 text-sm font-normal">
-                  Menu Principal
-                </span>
+                <span className="ml-3 text-slate-400 text-sm font-normal">Menu Principal</span>
               )}
               <ul className="flex flex-col gap-2">
                 <li className="w-full flex place-items-center">
                   <Link
                     href="/tenants/"
                     className={
-                      Styles.navitem +
-                      " " +
-                      (pathname === "/tenants" ? Styles.active : "")
-                    }
-                  >
+                      Styles.navitem + " " + (pathname === "/tenants" ? Styles.active : "")
+                    }>
                     <Image
-                      src={
-                        pathname === "/tenants"
-                          ? "/icons/home.svg"
-                          : "/icons/home_stroke.svg"
-                      }
+                      src={pathname === "/tenants" ? "/icons/home.svg" : "/icons/home_stroke.svg"}
                       alt="Home icon"
                       width={22}
                       height={23}
@@ -51,11 +42,8 @@ export default function SideBar() {
                       <Link
                         href="/agreements/"
                         className={
-                          Styles.navitem +
-                          " " +
-                          (pathname === "/agreements" ? Styles.active : "")
-                        }
-                      >
+                          Styles.navitem + " " + (pathname === "/agreements" ? Styles.active : "")
+                        }>
                         <Image
                           src={
                             pathname === "/agreements"
@@ -75,20 +63,15 @@ export default function SideBar() {
             </div>
             <div className="mt-20 flex flex-col gap-2">
               {!hideSideBar && (
-                <span className="ml-3 text-slate-400 text-sm font-normal">
-                  Preferências
-                </span>
+                <span className="ml-3 text-slate-400 text-sm font-normal">Preferências</span>
               )}
               <ul className="flex flex-col gap-2">
                 <li className="w-full flex place-items-center">
                   <Link
-                    href="#"
+                    href="/settings"
                     className={
-                      Styles.navitem +
-                      " " +
-                      (pathname === "/settings" ? Styles.active : "")
-                    }
-                  >
+                      Styles.navitem + " " + (pathname === "/settings" ? Styles.active : "")
+                    }>
                     <Image
                       src={
                         pathname === "/settings"
@@ -105,18 +88,9 @@ export default function SideBar() {
                 <li className="w-full flex place-items-center">
                   <Link
                     href="#"
-                    className={
-                      Styles.navitem +
-                      " " +
-                      (pathname === "/help" ? Styles.active : "")
-                    }
-                  >
+                    className={Styles.navitem + " " + (pathname === "/help" ? Styles.active : "")}>
                     <Image
-                      src={
-                        pathname === "/help"
-                          ? "/icons/help.svg"
-                          : "/icons/help_stroke.svg"
-                      }
+                      src={pathname === "/help" ? "/icons/help.svg" : "/icons/help_stroke.svg"}
                       alt="Options icon"
                       width={22}
                       height={23}
