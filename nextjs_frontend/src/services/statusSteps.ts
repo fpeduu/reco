66e55c142f6d421514e8fb07b9d8cfd9cec409d0
JDaniloC <jdsc@cin.ufpe.br>
2023-10-10 22:00:50 -0300

    Refactor chat logics

--- nextjs_frontend/src/services/statusSteps.ts
+++ nextjs_frontend/src/services/statusSteps.ts
@@ -2,14 +2,14 @@ import { StatusType } from "@/models/Acordos";
 
 export function getStatusStep(status: StatusType) {
   switch (status) {
-    case "Conversa iniciada":
-      return 0; // Este passo ocorre ao mesmo tempo que o 2°
     case "Primeira proposta":
-      return 1;
-    case "Segunda proposta":
       return 2;
+    case "Segunda proposta":
+      return 3;
+    case "Decisão do inadimplente":
+      return 3;
     case "Proposta do inadimplente":
-      return 3; // Este passo ocorre ao mesmo tempo que o 5°
+      return 3;
     case "Aguardando aprovação":
       return 4;
     case "Acordo recusado":
